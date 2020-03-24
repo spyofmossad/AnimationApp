5bbc5ed27ee387f51d77eae120855e88f38d6108
dmitry.navitski <arbalestindigo@gmail.com>
2020-03-24 17:15:01 +0300

    Initial Commit

---
+++ AnimationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Dmitry on 24.03.2020.
+//  Copyright © 2020 Dmitry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
